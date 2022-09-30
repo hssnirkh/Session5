e2da7c77cb4da7e53788fef4ab44afee7e2d777f
hssnirkh <hassan.irankhah@gmail.com>
2022-10-01 01:21:08 +0330

    mk 3.py

---
+++ 3.py
@@ -0,0 +1,14 @@
+mainList = []
+showList = []
+
+for i in range(0,10):
+	mainList.append(input("STR : "))
+chr = input("Enter chr : ")
+for j in range(0,len(mainList)):
+	c = mainList[j]
+	for z in range(0,len(c)):
+		if(c[1] == chr):
+			showList.append(j)
+			break
+
+print(mainList,showList)
